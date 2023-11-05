2df112c94018a51b4c1133abc4431fbc8f677d4e
ManticoreXL <zip3366@naver.com>
2023-11-06 01:57:00 +0900

    fix

--- BpTree.h
+++ BpTree.h
@@ -7,30 +7,32 @@
 #include <fstream>
 #include <iostream>
 
-class BpTree {
+class BpTree
+{
 private:
-	BpTreeNode* root;
-	int			order;		// m children
-	ofstream* fout;
+	BpTreeNode *root;
+	int order; // m children
+	ofstream *fout;
+
 public:
-	BpTree(ofstream *fout, int order = 3) {
+	BpTree(ofstream *fout, int order = 3)
+	{
 		root = NULL;
 		this->order = order;
 		this->fout = fout;
 	}
 	~BpTree();
 	/* essential */
-	bool Insert(LoanBookData* newData);
-	bool excessDataNode(BpTreeNode* pDataNode);
-	bool excessIndexNode(BpTreeNode* pIndexNode);
-	void splitDataNode(BpTreeNode* pDataNode);
-	void splitIndexNode(BpTreeNode* pIndexNode);
-	BpTreeNode* getRoot() { return root; }
-	BpTreeNode* searchDataNode(string name);
+	bool Insert(LoanBookData *newData);
+	bool excessDataNode(BpTreeNode *pDataNode);
+	bool excessIndexNode(BpTreeNode *pIndexNode);
+	void splitDataNode(BpTreeNode *pDataNode);
+	void splitIndexNode(BpTreeNode *pIndexNode);
+	BpTreeNode *getRoot() { return root; }
+	BpTreeNode *searchDataNode(string name);
 
 	bool searchBook(string name);
 	bool searchRange(string start, string end);
-
 };
 
 #endif
--- LoanBookHeap.h
+++ LoanBookHeap.h
@@ -5,21 +5,17 @@
 class LoanBookHeap
 {
 private:
-    LoanBookHeapNode* root;
+    LoanBookHeapNode *root;
 
 public:
-    LoanBookHeap() {
-        this->root = NULL;
-    };
-    ~LoanBookHeap() {
+    LoanBookHeap() { this->root = NULL; };
+    ~LoanBookHeap() {}
 
-    }
-    
-    void setRoot(LoanBookHeapNode* pN) { this->root = pN; }
-    LoanBookHeapNode* getRoot() { return root; }
+    void setRoot(LoanBookHeapNode *pN) { this->root = pN; }
+    LoanBookHeapNode *getRoot() { return root; }
 
-    void heapifyUp(LoanBookHeapNode* pN);
-    void heapifyDown(LoanBookHeapNode* pN);
-    
-    bool Insert(LoanBookData* data);
+    void heapifyUp(LoanBookHeapNode *pN);
+    void heapifyDown(LoanBookHeapNode *pN);
+
+    bool Insert(LoanBookData *data);
 };
