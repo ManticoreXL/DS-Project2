c0a613b519b6f9686b8bc71a94bab014331e8470
ManticoreXL <zip3366@naver.com>
2023-11-08 23:45:46 +0900

    heap

--- LoanBookHeap.cpp
+++ LoanBookHeap.cpp
@@ -10,4 +10,13 @@ void LoanBookHeap::heapifyDown(LoanBookHeapNode *pN)
 
 bool LoanBookHeap::Insert(LoanBookData *data)
 {
+    if (root == nullptr)
+    {
+        root = new LoanBookHeapNode(data);
+        return true;
+    }
+    else
+    {
+        LoanBookHeapNode *curr = root;
+    }
 }
